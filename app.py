2c0045e34c4821203129ed918fbbaac874303284
Moïse <60554715+KasMoise@users.noreply.github.com>
2025-06-19 20:18:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 st.header("Books Recommander System using Machine Learning")
 model=pickle.load(open('artifacts/model.pkl','rb'))
 books_name=pickle.load(open('artifacts/books_name.pkl','rb'))
-final_rating=pickle.load(open('artifacts/final_rating.pkl','rb'))
+##final_rating=pickle.load(open('artifacts/final_rating.pkl','rb'))
 book_pivot=pickle.load(open('artifacts/book_pivot.pkl','rb'))
 
 
